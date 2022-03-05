6119e664d9caeacba68dcbe5044aab75fbf3676a
Myoung <mhbae93@gmail.com>
2022-03-05 10:20:51 +0000

    added not existing route middleware

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import taskRouter from "./routes/tasks.js";
-import db from "./db/connect.js";
 import connectDB from "./db/connect.js";
+import notFound from "./middleware/not-found.js"
 
 const PORT = 5000;
 const app = express();
@@ -12,8 +12,10 @@ connectDB();
 app.use(express.json());
 app.use(express.static("./public"));
 
+
 //ROUTES
 app.use("/api/v1/tasks", taskRouter);
+app.use(notFound)
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}...`);
