aaf88bfa63b2b263722f7465d44ee7d5d25c21d2
Myoung <mhbae93@gmail.com>
2022-03-05 07:24:06 +0000

    you can now fetch all tasks from the database and fetch a single task by id

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,24 +1,48 @@
+import Task from "../models/Task.js";
 
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
 
-const getAllTasks = (req, res) => {
-  res.send('all items from the file')
-}
+const createTask = async (req, res) => {
+  try {
+    const { name, completed } = req.body;
+    let task = new Task({
+      name,
+      completed,
+    });
 
-const createTask = (req, res) => {
-  res.send("create task")
-}
+    const newTask = await task.save();
+    res.status(201).json({ newTask });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
 
-const getTask = (req, res) => {
-  res.send("get single task")
-}
+const getTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    res.status(201).json({ task });
+    if (!task) {
+      return res.status(404).json({ mgs: `No task with id: ${task} ` });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
 
 const updateTask = (req, res) => {
-  res.send("update task")
-}
+  res.send("update task");
+};
 
 const deleteTask = (req, res) => {
-  res.send("delete task")
-}
+  res.send("delete task");
+};
 
-
-export {getAllTasks, createTask, getTask, updateTask, deleteTask}
+export { getAllTasks, createTask, getTask, updateTask, deleteTask };
--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: [20, "name cannot be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const Task = mongoose.model("Task", TaskSchema);
